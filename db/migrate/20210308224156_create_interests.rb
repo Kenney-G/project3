4af541868b981c25e34e584529f27a4909c874a2
kendofriendo <goadke@g.cofc.edu>
2021-03-08 17:45:51 -0500

    generate interests

---
+++ db/migrate/20210308224156_create_interests.rb
@@ -0,0 +1,12 @@
+class CreateInterests < ActiveRecord::Migration[6.1]
+  def change
+    create_table :interests do |t|
+      t.string :name
+      t.text :description
+      t.belongs_to :user, null: false, foreign_key: true
+      t.belongs_to :category, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
